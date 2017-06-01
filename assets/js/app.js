0bb05f3b583c429c3b97a8ff359edda92a89eba0
Syrimande <caterinadasilva@gmail.com>
2017-06-01 12:51:25 -0400

    Funcionalidad y estilos modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
+// // EFECTO SCROLL DEL NAV // //
 /* Código de Liliana Kastilio: https://codepen.io/lili2311/pen/dJjuL< */
-
 window.addEventListener('scroll', function () {
   const distanceY = window.pageYOffset || document.documentElement.scrollTop,
   shrinkOn = 200,
@@ -11,3 +11,21 @@ window.addEventListener('scroll', function () {
     headerEl.classList.remove("smaller");
   }
 });
+
+// // MODAL // //
+(function modal(){
+	var boxes = Array.from(document.getElementsByClassName("project"));
+	var modal = document.getElementById("work-modal");
+	var modalBody = document.getElementsByClassName('modal-body')[0];
+	var close = document.getElementById("close");
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			event.preventDefault();
+			modalBody.innerHTML = box.innerHTML;
+			modal.classList.remove("hidden");
+			close.addEventListener("click",function(){
+				modal.classList.add("hidden");
+			});
+		});		
+	});
+})();
