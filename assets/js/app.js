e12d9ec0c7720c1cb40ff0761636531653296634
Syrimande <caterinadasilva@gmail.com>
2017-06-01 21:47:54 -0400

    Funcionalidad formulario

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,99 @@ window.addEventListener('scroll', function () {
 		});		
 	});
 })();
+
+// // VALIDACIÓN FORMULARIO // //
+(function validateForm(){
+	var sendBtn = document.getElementById('sendBtn');
+	sendBtn.addEventListener('click', function() {
+		event.preventDefault();
+	// ELIMINAR SMALLS DE VALIDACIONES ANTERIORES
+		document.querySelectorAll(".form-input small").forEach(function(small) {small.remove()});
+	// VALORES VALUE
+		var name = document.getElementById('name').value;
+		var email = document.getElementById('email').value;
+		var phone = document.getElementById('phone').value;
+		var textarea = document.getElementById('textarea').value;
+	// CARACTERES VÁLIDOS // REGULAR EXPRESSIONS
+		var validName = /^[A-Za-z]*/;
+		var validPhone = /^56(?=[1-9]\d{0,2}[1-9])(?=\d{2,15}$)\d+$/;
+		var validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	////// NOMBRE
+		// VERIFICACIÓN DE CAMPO OBLIGATORIO
+		if (name === "") {
+			var rellenarText = document.createTextNode("Por favor, rellene este campo.");
+			var labelError = document.createElement('small');
+			labelError.classList.add('label', 'error');
+			labelError.appendChild(rellenarText);
+			document.getElementsByClassName('form-input')[0].appendChild(labelError);
+			console.log("Nombre:" + name);
+		} else {
+		// IMPRIMIR Y VACIAR CAMPO
+			console.log("Nombre:" + name);
+			document.getElementById('name').value = "";
+		}
+	////// EMAIL
+		// VERIFICACIÓN DE CAMPOS VACÍOS	
+		if (email === "") {
+			var rellenarText = document.createTextNode("Por favor, rellene este campo.");
+			var labelError = document.createElement('small');
+			labelError.classList.add('label', 'error');
+			labelError.appendChild(rellenarText);
+			document.getElementsByClassName('form-input')[1].appendChild(labelError);
+			console.log("Correo: " + email);
+		}
+		// VERIFICACIÓN DE CARACTERES REQUERIDOS
+		else if(!validEmail.test(email)) {
+			var correoText = document.createTextNode("Este correo no es válido.");
+			var labelError = document.createElement('small');
+			labelError.classList.add('label', 'error');
+			labelError.appendChild(correoText);
+			document.getElementsByClassName('form-input')[1].appendChild(labelError);
+			console.log("Correo: " + email + ". Tiene caracteres no permitidos.");
+		}
+		// IMPRIMIR Y VACIAR CAMPO
+		else {
+			console.log("Correo: " + email);
+			document.getElementById('email').value = "";
+		}
+	////// TELÉFONO
+		// VERIFICACIÓN DE CAMPOS VACÍOS	
+		if (phone === "") {
+			var rellenarText = document.createTextNode("Por favor, rellene este campo.");
+			var labelError = document.createElement('small');
+			labelError.classList.add('label', 'error');
+			labelError.appendChild(rellenarText);
+			document.getElementsByClassName('form-input')[2].appendChild(labelError);
+			console.log("Phone: " + phone);
+		}
+		// VERIFICACIÓN DE CARACTERES REQUERIDOS
+		else if(!validPhone.test(phone)) {
+			var phoneText = document.createTextNode("Este número no es válido.");
+			var labelError = document.createElement('small');
+			labelError.classList.add('label', 'error');
+			labelError.appendChild(phoneText);
+			document.getElementsByClassName('form-input')[2].appendChild(labelError);
+			console.log("Teléfono: " + phone + ". Tiene caracteres no permitidos.");
+		}
+		// IMPRIMIR Y VACIAR CAMPO
+		else {
+			console.log("Teléfono: " + phone);
+			document.getElementById('phone').value = "";
+		}
+	////// MENSAJE
+		// VERIFICACIÓN DE CAMPO OBLIGATORIO	
+		if (textarea === "") {
+			var rellenarText = document.createTextNode("Por favor, rellene este campo.");
+			var labelError = document.createElement('small');
+			labelError.classList.add('label', 'error');
+			labelError.appendChild(rellenarText);
+			document.getElementsByClassName('form-input')[3].appendChild(labelError);
+			console.log("Mensaje:" + textarea);
+		} else {
+		// IMPRIMIR Y VACIAR CAMPO
+			console.log("Mensaje:" + textarea);
+			document.getElementById('textarea').value = "";
+		}
+	////// FIN
+	});
+})();
